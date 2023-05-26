9c114c122c0d6e184653957d7fdedda8d17db7e6
Johnny Tm <36928647+socalledJT@users.noreply.github.com>
2023-05-26 21:04:17 +0200

    Added transitions
    Added scroll transitions for all website elements, also styled the scrollbar

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,40 @@
- // Get the navbar links
- var navbarLinks = document.getElementsByClassName("navbar")[0].getElementsByTagName("a");
+// Get the navbar links
+var navbarLinks = document.getElementsByClassName("navbar")[0].getElementsByTagName("a");
     
- // Set the home icon as active by default
- navbarLinks[0].classList.add("active");
+// Set the home icon as active by default
+navbarLinks[0].classList.add("active");
  
- // Add event listeners to each navbar link
- for (var i = 0; i < navbarLinks.length; i++) {
-   navbarLinks[i].addEventListener("click", function() {
-     // Remove the "active" class from all links
-     for (var j = 0; j < navbarLinks.length; j++) {
-       navbarLinks[j].classList.remove("active");
-     }
+// Add event listeners to each navbar link
+for (var i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function() {
+    // Remove the "active" class from all links
+    for (var j = 0; j < navbarLinks.length; j++) {
+      navbarLinks[j].classList.remove("active");
+    }
  
-     // Add the "active" class to the clicked link
-     this.classList.add("active");
-   });
- }
+    // Add the "active" class to the clicked link
+    this.classList.add("active");
+  });
+}
+
+
+window.addEventListener('scroll', fadeInElements);
+
+function fadeInElements() {
+  var elements = document.querySelectorAll('.fade-in');
+
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var elementPosition = element.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight;
+
+    if (elementPosition < screenHeight) {
+      element.style.opacity = 1;
+      element.classList.remove('.fade-in');
+    }
+  }
+}
+
+$("document").ready( function () {
+  alert('This website is still under construction. \nPlease understand you may experience missing information and/or browser issues');
+});
