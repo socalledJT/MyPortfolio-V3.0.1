7f8ff2aa2ffb331413a72a0c1d276b9ee0a252f9
Johnny Tm <36928647+socalledJT@users.noreply.github.com>
2023-11-08 13:54:42 +0100

    Minor bug fixes
    ~ Fixed bug where mySkills icons would show different sizes.
    ~ Made Social-icons change color when user reaches end of page
    ~ Deleted redundant images from ../images.

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,61 @@
+// Function to get the navigation bar working with changing colors
 // Get the navbar links
-var navbarLinks = document.getElementsByClassName("navbar")[0].getElementsByTagName("a");
-    
+var navbarLinks = document
+  .getElementsByClassName("navbar")[0]
+  .getElementsByTagName("a");
+
 // Set the home icon as active by default
 navbarLinks[0].classList.add("active");
- 
+
 // Add event listeners to each navbar link
 for (var i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function() {
+  navbarLinks[i].addEventListener("click", function () {
     // Remove the "active" class from all links
     for (var j = 0; j < navbarLinks.length; j++) {
       navbarLinks[j].classList.remove("active");
     }
- 
+
     // Add the "active" class to the clicked link
     this.classList.add("active");
   });
 }
 
+//Function to change the colors of social-links when the user reaches the end of the page
+function changeIconColorOnScroll() {
+  var icons = document.querySelectorAll(".social-icon");
+  var pageHeight = document.documentElement.scrollHeight - window.innerHeight;
 
+  // Add a scroll event listener
+  window.addEventListener("scroll", function () {
+    // Get the current scroll position
+    var scrollPosition = window.scrollY;
+
+    // Check if the user has reached the bottom of the page
+    if (scrollPosition >= pageHeight) {
+      // Apply green color to the icons
+      icons.forEach(function (icon) {
+        icon.style.transition = "color 0.3s";
+        icon.style.color = "#00E35D";
+      });
+    } else {
+      // Reset the color to white
+      icons.forEach(function (icon) {
+        icon.style.transition = "color 0.3s";
+        icon.style.color = "white";
+      });
+    }
+  });
+}
+
+// Call the function to start monitoring the scroll position
+changeIconColorOnScroll();
+
+//Function to fade elements in when the user scrolls down
 //Scroll effect animation activation
-window.addEventListener('scroll', fadeInElements);
+window.addEventListener("scroll", fadeInElements);
 
 function fadeInElements() {
-  var elements = document.querySelectorAll('.fade-in');
+  var elements = document.querySelectorAll(".fade-in");
 
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -31,7 +64,7 @@ function fadeInElements() {
 
     if (elementPosition < screenHeight) {
       element.style.opacity = 1;
-      element.classList.remove('.fade-in');
+      element.classList.remove(".fade-in");
     }
   }
 }
